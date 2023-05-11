726289ffb78dbc100fa60f50064216d146985680
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-11 15:29:48 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -2,6 +2,7 @@ import streamlit as st
 from notas_fiscais import notas_fiscais
 
 
+def main():
 # Configuração do layout
 st.set_page_config(page_title="Aplicativo de Análise Fiscal")
 
@@ -11,6 +12,6 @@ st.title("Aplicativo de Análise Fiscal")
 # Criação das guias na barra lateral
 opcao = st.sidebar.selectbox("Selecione uma opção","Notas Fiscais")
 
-if opcao == "Notas Fiscais":
-    notas_fiscais()
+ if notasfiscais_choice == "Notas Fiscais":
+            notas_fiscais()
 
