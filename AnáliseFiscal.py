2d9f0c9f72d98f8397767351b8b42772d199461f
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-11 15:38:46 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -1,10 +1,5 @@
 import streamlit as st
 from notas_fiscais import notas_fiscais
-from notas_complementares import notas_complementares
-from efd import efd
-from cat import cat
-from conversao_codigo import conversao_codigo
-from analise_fiscal import analise_fiscal
 
 def main():
     st.title("Aplicativo de Análise Fiscal")
