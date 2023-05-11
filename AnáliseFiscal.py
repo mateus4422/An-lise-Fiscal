fd6ef8bb563a84446cbb797cfb854a43d6946e5d
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-11 15:43:54 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -2,40 +2,33 @@ import streamlit as st
 from notas_fiscais import notas_fiscais
 #from notas_complementares import notas_complementares
 #from efd import efd
-#from cat import cat'
-#from conversao_codigo import conversao_codigo'
+#from cat import cat
+#from conversao_codigo import conversao_codigo
 #from analise_fiscal import analise_fiscal
 
 def main():
     st.title("Aplicativo de Análise Fiscal")
 
     menu_options = ["Notas Fiscais", "Notas Complementares", "EFD", "CAT", "Conversão de Código (NF Original)", "Análise Fiscal"]
-    choices = st.sidebar.multiselect("Menu", menu_options)
+    choice = st.sidebar.selectbox("Menu", menu_options)
 
-    for choice in choices:
-        if choice == "Notas Fiscais":
-            st.write("## Notas Fiscais")
-            notas_fiscais()
+    if choice == "Notas Fiscais":
+        notas_fiscais()
 
-        if choice == "Notas Complementares":
-            st.write("## Notas Complementares")
-            notas_complementares()
+    if choice == "Notas Complementares":
+        notas_complementares()
 
-        if choice == "EFD":
-            st.write("## EFD")
-            efd()
+    if choice == "EFD":
+        efd()
 
-        if choice == "CAT":
-            st.write("## CAT")
-            cat()
+    if choice == "CAT":
+        cat()
 
-        if choice == "Conversão de Código (NF Original)":
-            st.write("## Conversão de Código (NF Original)")
-            conversao_codigo()
+    if choice == "Conversão de Código (NF Original)":
+        conversao_codigo()
 
-        if choice == "Análise Fiscal":
-            st.write("## Análise Fiscal")
-            analise_fiscal()
+    if choice == "Análise Fiscal":
+        analise_fiscal()
 
 if __name__ == "__main__":
     main()
