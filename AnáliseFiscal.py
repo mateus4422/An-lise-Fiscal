211e11bb737557b708d272ccafa734929c1a9771
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-11 15:45:19 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -9,8 +9,9 @@ from notas_fiscais import notas_fiscais
 def main():
     st.title("Aplicativo de Análise Fiscal")
 
+    st.sidebar.title("Menu")
     menu_options = ["Notas Fiscais", "Notas Complementares", "EFD", "CAT", "Conversão de Código (NF Original)", "Análise Fiscal"]
-    choice = st.sidebar.selectbox("Menu", menu_options)
+    choice = st.sidebar.radio("Selecione uma opção", menu_options)
 
     if choice == "Notas Fiscais":
         notas_fiscais()
