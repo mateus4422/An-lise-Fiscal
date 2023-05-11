d4bd55f1f58a26307ec80f76548990d8bac21ff0
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-11 15:42:48 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from notas_fiscais import notas_fiscais
-'from notas_complementares import notas_complementares'
-'from efd import efd'
-'from cat import cat'
-'from conversao_codigo import conversao_codigo'
-'from analise_fiscal import analise_fiscal'
+#from notas_complementares import notas_complementares
+#from efd import efd
+#from cat import cat'
+#from conversao_codigo import conversao_codigo'
+#from analise_fiscal import analise_fiscal
 
 def main():
     st.title("Aplicativo de Análise Fiscal")
@@ -14,27 +14,27 @@ def main():
 
     for choice in choices:
         if choice == "Notas Fiscais":
-            st.write("Notas Fiscais")
+            st.write("## Notas Fiscais")
             notas_fiscais()
 
         if choice == "Notas Complementares":
-            st.write("Notas Complementares")
+            st.write("## Notas Complementares")
             notas_complementares()
 
         if choice == "EFD":
-            st.write("EFD")
+            st.write("## EFD")
             efd()
 
         if choice == "CAT":
-            st.write("CAT")
+            st.write("## CAT")
             cat()
 
         if choice == "Conversão de Código (NF Original)":
-            st.write("Conversão de Código (NF Original)")
+            st.write("## Conversão de Código (NF Original)")
             conversao_codigo()
 
         if choice == "Análise Fiscal":
-            st.write("Análise Fiscal")
+            st.write("## Análise Fiscal")
             analise_fiscal()
 
 if __name__ == "__main__":
