1ee6f655aa3a36572cdb1fac65a94fcf88da6a4e
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-11 15:35:59 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -2,16 +2,12 @@ import streamlit as st
 from notas_fiscais import notas_fiscais
 
 def main():
-    menu_options = ["Notas Fiscais", "Notas Complementares", "EFD", "CAT"]
-    choice = st.sidebar.selectbox("Menu", menu_options)
- 
-    if choice == "Notas Fiscais":
-        notasfiscais_options = ["Selecione uma opção", "Registro 1100"]
-        notasfiscais_choice = st.sidebar.selectbox("Notas Fiscais", notasfiscais_options)
-        
-        if notasfiscais_choice == "Registro 1100":
-            st.subheader("Notas Fiscais")
-            notas_fiscais()
+    st.title("Aplicativo de Análise Fiscal")
+
+    st.sidebar.markdown("Notas Fiscais")
+    st.sidebar.markdown("Selecione a opção:")
+    
+    notas_fiscais()
 
 if __name__ == "__main__":
     main()
