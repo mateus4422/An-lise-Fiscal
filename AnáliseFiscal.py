47104c832189b3c1e2c7ff7a40c256980937dc9c
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-11 15:38:07 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -1,13 +1,29 @@
 import streamlit as st
 from notas_fiscais import notas_fiscais
+from notas_complementares import notas_complementares
+from efd import efd
+from cat import cat
+from conversao_codigo import conversao_codigo
+from analise_fiscal import analise_fiscal
 
 def main():
     st.title("Aplicativo de Análise Fiscal")
 
-    st.sidebar.markdown("Notas Fiscais")
-    st.sidebar.markdown("Selecione a opção:")
-    
-    notas_fiscais()
+    menu_options = ["Notas Fiscais", "Notas Complementares", "EFD", "CAT", "Conversão de Código (NF Original)", "Análise Fiscal"]
+    choice = st.sidebar.selectbox("Menu", menu_options)
+
+    if choice == "Notas Fiscais":
+        notas_fiscais()
+    elif choice == "Notas Complementares":
+        notas_complementares()
+    elif choice == "EFD":
+        efd()
+    elif choice == "CAT":
+        cat()
+    elif choice == "Conversão de Código (NF Original)":
+        conversao_codigo()
+    elif choice == "Análise Fiscal":
+        analise_fiscal()
 
 if __name__ == "__main__":
     main()
