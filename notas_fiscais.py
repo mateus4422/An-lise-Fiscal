5bcf9bbe80c31ae8f14dc2cc442e7beb7005fd18
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-12 11:13:05 -0300

    Update notas_fiscais.py

--- notas_fiscais.py
+++ notas_fiscais.py
@@ -3,62 +3,62 @@ import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def notas_fiscais():
+# Função para extrair os dados do XML
+def extract_data(file):
+    # Cria um objeto BeautifulSoup com o parser XML
+    bs = BeautifulSoup(file, 'xml')
 
-    # Função para extrair os dados do XML
-    def extract_data(file):
-        # Cria um objeto BeautifulSoup
-        bs = BeautifulSoup(file, 'lxml')
+    # Extrai a chave de acesso da nota fiscal
+    chave_nfe = bs.find('infProt').get('chNFe') if bs.find('infProt') else None
+    
+    # Inicializa uma lista vazia para armazenar os dados de todos os produtos
+    data = []
+    
+    # Itera sobre todos os elementos <det>
+    for det in bs.find_all('det'):
+        # Extrai os dados necessários
+        product_data = {
+            'Chave do produto': chave_nfe,
+            'Código de Item': det.get('nItem'),
+            'Data de emissão': bs.find('ide').find('dhEmi').string if bs.find('ide') and bs.find('ide').find('dhEmi') else None,
+            'CFOP do produto': det.find('prod').find('CFOP').string if det.find('prod') and det.find('prod').find('CFOP') else None,
+            'NCM do produto': det.find('prod').find('NCM').string if det.find('prod') and det.find('prod').find('NCM') else None,
+            'Código do produto': det.find('prod').find('cProd').string if det.find('prod') and det.find('prod').find('cProd') else None,
+            'Descrição do Produto': det.find('prod').find('xProd').string if det.find('prod') and det.find('prod').find('xProd') else None,
+            'Quantidade do produto': det.find('prod').find('qCom').string if det.find('prod') and det.find('prod').find('qCom') else None,
+            'EAN do produto': det.find('prod').find('cEAN').string if det.find('prod') and det.find('prod').find('cEAN') else None,
+            'Valor do Produto': det.find('prod').find('vProd').string if det.find('prod') and det.find('prod').find('vProd') else None,
+            'ICMS do produto': det.find('ICMS60').find('vICMSSubstituto').string if det.find('ICMS60') and det.find('ICMS60').find('vICMSSubstituto') else None,
+            'ICMS ret do produto': det.find('ICMS60').find('vICMSSTRet').string if det.find('ICMS60') and det.find('ICMS60').find('vICMSSTRet') else None,
+        }
+        
+        # Adiciona os dados do produto à lista
+        data.append(product_data)
+    
+    return data
+# Cria a interface do Streamlit
+st.title('Carregador de Notas Fiscais')
 
-        # Inicializa uma lista vazia para armazenar os dados de todos os produtos
-        data = []
+# Cria um seletor de arquivos para vários arquivos
+files = st.file_uploader('Upload your XML files', type=['xml'], accept_multiple_files=True)
 
-        # Extrai a chave de acesso da nota fiscal
-        chave_nfe = bs.find('infProt').get('chNFe') if bs.find('infProt') else None
+# Se algum ou mais arquivos foram carregados
+if files:
+    # Inicializa um DataFrame vazio
+    data = pd.DataFrame()
 
-        # Itera sobre todos os elementos <det>
-        for det in bs.find_all('det'):
-            # Extrai os dados necessários
-            product_data = {
-                'Chave do produto': chave_nfe,
-                'Código de Item': det.get('nItem'),
-                'Data de emissão': bs.find('ide').dhEmi.string,
-                'CFOP do produto': det.find('prod').CFOP.string,
-                'NCM do produto': det.find('prod').NCM.string,
-                'Código do produto': det.find('prod').cProd.string,
-                'Descrição do Produto': det.find('prod').xProd.string,
-                'Quantidade do produto': det.find('prod').qCom.string,
-                'EAN do produto': det.find('prod').cEAN.string,
-                'Valor do Produto': det.find('prod').vProd.string,
-                'ICMS do produto': det.find('ICMS60').vICMSSubstituto.string if det.find('ICMS60') else None,
-                'ICMS ret do produto': det.find('ICMS60').vICMSSTRet.string if det.find('ICMS60') else None,
-            }
+    # Itera sobre os arquivos carregados
+    for file in files:
+        # Extrai os dados do arquivo XML
+        file_data = extract_data(file)
+        
+        # Cria um DataFrame com os dados do arquivo
+        file_df = pd.DataFrame(file_data)
+        
+        # Adiciona os dados do arquivo ao DataFrame geral
+        data = data.append(file_df, ignore_index=True)
 
-            # Adiciona os dados do produto à lista
-            data.append(product_data)
+    # Exibe os dados
+    st.write(data)
 
-        return data
-    # Cria a interface do Streamlit
-    st.title('Carregador de Notas Fiscais')
 
-    # Cria um seletor de arquivos para vários arquivos
-    files = st.file_uploader('Upload your XML files', type=['xml'], accept_multiple_files=True)
-
-    # Se algum ou mais arquivos foram carregados
-    if files:
-        # Inicializa um DataFrame vazio
-        data = pd.DataFrame()
-
-        # Itera sobre os arquivos carregados
-        for file in files:
-            # Extrai os dados do arquivo XML
-            file_data = extract_data(file)
-
-            # Cria um DataFrame com os dados do arquivo
-            file_df = pd.DataFrame(file_data)
-
-            # Adiciona os dados do arquivo ao DataFrame geral
-            data = data.append(file_df, ignore_index=True)
-
-        # Exibe os dados
-        st.write(data)
