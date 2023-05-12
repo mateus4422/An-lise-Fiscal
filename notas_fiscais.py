139f141dd01a6b7b377d0a2d89ab8a04fd7c230e
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-12 10:32:13 -0300

    Update notas_fiscais.py

--- notas_fiscais.py
+++ notas_fiscais.py
@@ -3,39 +3,40 @@ import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup
 
-# Função para extrair os dados do XML
-def extract_data(file):
-    # Cria um objeto BeautifulSoup
-    bs = BeautifulSoup(file, 'lxml')
+def notas_fiscais():
+    # Função para extrair os dados do XML
+    def extract_data(file):
+        # Cria um objeto BeautifulSoup
+        bs = BeautifulSoup(file, 'lxml')
 
-    # Extrai os dados necessários
-    data = {
-        'Chave do produto': bs.find('infprot').get('chnfe'),
-        'Código de Item': bs.find('det').get('nitem'),
-        'Data de emissão': bs.find('ide').dhemi.string,
-        'CFOP do produto': bs.find('prod').cfop.string,
-        'NCM do produto': bs.find('prod').ncm.string,
-        'Código do produto': bs.find('prod').cprod.string,
-        'Descrição do Produto': bs.find('prod').xprod.string,
-        'Quantidade do produto': bs.find('prod').qcom.string,
-        'EAN do produto': bs.find('prod').cean.string,
-        'Valor do Produto': bs.find('prod').vprod.string,
-        'ICMS do produto': bs.find('icms60').vicmssubstituto.string if bs.find('icms60') else None,
-        'ICMS ret do produto': bs.find('icms60').vicmsstret.string if bs.find('icms60') else None,
-    }
+        # Extrai os dados necessários
+        data = {
+            'Chave do produto': bs.find('infprot').get('chnfe'),
+            'Código de Item': bs.find('det').get('nitem'),
+            'Data de emissão': bs.find('ide').dhemi.string,
+            'CFOP do produto': bs.find('prod').cfop.string,
+            'NCM do produto': bs.find('prod').ncm.string,
+            'Código do produto': bs.find('prod').cprod.string,
+            'Descrição do Produto': bs.find('prod').xprod.string,
+            'Quantidade do produto': bs.find('prod').qcom.string,
+            'EAN do produto': bs.find('prod').cean.string,
+            'Valor do Produto': bs.find('prod').vprod.string,
+            'ICMS do produto': bs.find('icms60').vicmssubstituto.string if bs.find('icms60') else None,
+            'ICMS ret do produto': bs.find('icms60').vicmsstret.string if bs.find('icms60') else None,
+        }
 
-    # Retorna os dados como um DataFrame
-    return pd.DataFrame([data])
-# Cria a interface do Streamlit
-st.title('Carregador de Notas Fiscais')
+        # Retorna os dados como um DataFrame
+        return pd.DataFrame([data])
+    # Cria a interface do Streamlit
+    st.title('Carregador de Notas Fiscais')
 
-# Cria um seletor de arquivos
-file = st.file_uploader('Upload your XML file', type=['xml'])
+    # Cria um seletor de arquivos
+    file = st.file_uploader('Upload your XML file', type=['xml'])
 
-# Se um arquivo foi carregado
-if file is not None:
-    # Extrai os dados do arquivo
-    data = extract_data(file)
+    # Se um arquivo foi carregado
+    if file is not None:
+        # Extrai os dados do arquivo
+        data = extract_data(file)
 
-    # Exibe os dados
-    st.write(data)
+        # Exibe os dados
+        st.write(data)
