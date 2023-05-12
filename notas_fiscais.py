f446bd824293df10db0a82b3a0d43da822b94ee6
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-12 10:59:37 -0300

    Update notas_fiscais.py

--- notas_fiscais.py
+++ notas_fiscais.py
@@ -3,52 +3,49 @@ import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def notas_fiscais():
 # Função para extrair os dados do XML
-    def extract_data(file):
-        # Cria um objeto BeautifulSoup
-        bs = BeautifulSoup(file, 'lxml')
+def extract_data(file):
+    # Cria um objeto BeautifulSoup
+    bs = BeautifulSoup(file, 'lxml')
+    
+    # Extrai os dados necessários
+    data = {
+        'Chave do produto': bs.find('infprot').get('chnfe') if bs.find('infprot') else None,
+        'Código de Item': bs.find('det').get('nitem') if bs.find('det') else None,
+        'Data de emissão': bs.find('ide').dhemi.string if bs.find('ide') else None,
+        'CFOP do produto': bs.find('prod').cfop.string if bs.find('prod') else None,
+        'NCM do produto': bs.find('prod').ncm.string if bs.find('prod') else None,
+        'Código do produto': bs.find('prod').cprod.string if bs.find('prod') else None,
+        'Descrição do Produto': bs.find('prod').xprod.string if bs.find('prod') else None,
+        'Quantidade do produto': bs.find('prod').qcom.string if bs.find('prod') else None,
+        'EAN do produto': bs.find('prod').cean.string if bs.find('prod') else None,
+        'Valor do Produto': bs.find('prod').vprod.string if bs.find('prod') else None,
+        'ICMS do produto': bs.find('icms60').vicmssubstituto.string if bs.find('icms60') else None,
+        'ICMS ret do produto': bs.find('icms60').vicmsstret.string if bs.find('icms60') else None,
+    }
+    
+    return data
+# Cria a interface do Streamlit
+st.title('Carregador de Notas Fiscais')
 
-        # Inicializa uma lista vazia para armazenar os dados de todos os produtos
-        data = []
+# Cria um seletor de arquivos para vários arquivos
+files = st.file_uploader('Upload your XML files', type=['xml'], accept_multiple_files=True)
 
-        # Extrai a chave de acesso da nota fiscal
-        chave_nfe = bs.find('infprot').chnfe.string if bs.find('infprot') and bs.find('infprot').chnfe else None
+# Se algum ou mais arquivos foram carregados
+if files:
+    # Inicializa uma lista vazia para armazenar os dados
+    all_data = []
 
-        # Itera sobre todos os elementos <det>
-        for det in bs.find_all('det'):
-            # Extrai os dados necessários
-            product_data = {
-                'Chave do produto': chave_nfe,
-                'Código de Item': det.get('nitem'),
-                'Data de emissão': bs.find('ide').dhemi.string,
-                'CFOP do produto': det.prod.cfop.string,
-                'NCM do produto': det.prod.ncm.string,
-                'Código do produto': det.prod.cprod.string,
-                'Descrição do Produto': det.prod.xprod.string,
-                'Quantidade do produto': det.prod.qcom.string,
-                'EAN do produto': det.prod.cean.string,
-                'Valor do Produto': det.prod.vprod.string,
-                'ICMS do produto': det.find('icms60').vicmssubstituto.string if det.find('icms60') else None,
-                'ICMS ret do produto': det.find('icms60').vicmsstret.string if det.find('icms60') else None,
-            }
-
-            # Adiciona os dados do produto à lista
-            data.append(product_data)
-
-        # Retorna os dados como um DataFrame
-        return pd.DataFrame(data)
-
-    # Cria a interface do Streamlit
-    st.title('Carregador de Notas Fiscais')
-
-    # Cria um seletor de arquivos
-    file = st.file_uploader('Upload your XML file', type=['xml'])
-
-    # Se um arquivo foi carregado
-    if file is not None:
-        # Extrai os dados do arquivo
+    # Itera sobre os arquivos carregados
+    for file in files:
+        # Extrai os dados do arquivo XML
         data = extract_data(file)
 
-        # Exibe os dados
-        st.write(data)
+        # Adiciona os dados à lista
+        all_data.append(data)
+
+    # Cria um DataFrame com os dados
+    df = pd.DataFrame(all_data)
+
+    # Exibe os dados
+    st.write(df)
