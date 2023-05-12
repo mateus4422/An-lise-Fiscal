bb5bca48867f09d77ffb82af47da10be1b47c6c6
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-12 10:56:13 -0300

    Update notas_fiscais.py

--- notas_fiscais.py
+++ notas_fiscais.py
@@ -3,46 +3,51 @@ import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def notas_fiscais()
-    # Função para extrair os dados do XML
-    def extract_data(file):
-        # Cria um objeto BeautifulSoup usando o parser XML (lxml)
-        bs = BeautifulSoup(file, 'xml')
-
+# Função para extrair os dados do XML
+def extract_data(file):
+    # Cria um objeto BeautifulSoup
+    bs = BeautifulSoup(file, 'lxml')
+    
+    # Inicializa uma lista vazia para armazenar os dados de todos os produtos
+    data = []
+    
+    # Extrai a chave de acesso da nota fiscal
+    chave_nfe = bs.find('infprot').chnfe.string if bs.find('infprot') and bs.find('infprot').chnfe else None
+    
+    # Itera sobre todos os elementos <det>
+    for det in bs.find_all('det'):
         # Extrai os dados necessários
-        data = {
-            'Chave do produto': bs.find('infProt').get('chNFe') if bs.find('infProt') else None,
-            'Código de Item': bs.find('det').get('nItem') if bs.find('det') else None,
-            'Data de emissão': bs.find('ide').dhEmi.string if bs.find('ide') else None,
-            'CFOP do produto': bs.find('prod').CFOP.string if bs.find('prod') else None,
-            'NCM do produto': bs.find('prod').NCM.string if bs.find('prod') else None,
-            'Código do produto': bs.find('prod').cProd.string if bs.find('prod') else None,
-            'Descrição do Produto': bs.find('prod').xProd.string if bs.find('prod') else None,
-            'Quantidade do produto': bs.find('prod').qCom.string if bs.find('prod') else None,
-            'EAN do produto': bs.find('prod').cEAN.string if bs.find('prod') else None,
-            'Valor do Produto': bs.find('prod').vProd.string if bs.find('prod') else None,
-            'ICMS do produto': bs.find('ICMS60').vICMSSubstituto.string if bs.find('ICMS60') else None,
-            'ICMS ret do produto': bs.find('ICMS60').vICMSSTRet.string if bs.find('ICMS60') else None,
+        product_data = {
+            'Chave do produto': chave_nfe,
+            'Código de Item': det.get('nitem'),
+            'Data de emissão': bs.find('ide').dhemi.string,
+            'CFOP do produto': det.prod.cfop.string,
+            'NCM do produto': det.prod.ncm.string,
+            'Código do produto': det.prod.cprod.string,
+            'Descrição do Produto': det.prod.xprod.string,
+            'Quantidade do produto': det.prod.qcom.string,
+            'EAN do produto': det.prod.cean.string,
+            'Valor do Produto': det.prod.vprod.string,
+            'ICMS do produto': det.find('icms60').vicmssubstituto.string if det.find('icms60') else None,
+            'ICMS ret do produto': det.find('icms60').vicmsstret.string if det.find('icms60') else None,
         }
+        
+        # Adiciona os dados do produto à lista
+        data.append(product_data)
+    
+    # Retorna os dados como um DataFrame
+    return pd.DataFrame(data)
 
-        # Retorna os dados como um DataFrame
-        return pd.DataFrame([data])
+# Cria a interface do Streamlit
+st.title('Carregador de Notas Fiscais')
 
-    # Cria a interface do Streamlit
-    st.title('Carregador de Notas Fiscais')
+# Cria um seletor de arquivos
+file = st.file_uploader('Upload your XML file', type=['xml'])
 
-    # Cria um seletor de arquivos para vários arquivos
-    files = st.file_uploader('Upload your XML files', type=['xml'], accept_multiple_files=True)
+# Se um arquivo foi carregado
+if file is not None:
+    # Extrai os dados do arquivo
+    data = extract_data(file)
 
-    # Se algum ou mais arquivos foram carregados
-    if files:
-        # Inicializa um DataFrame vazio
-        data = pd.DataFrame()
-
-        # Loop por todos os arquivos e extrai os dados
-        for file in files:
-            file_data = extract_data(file)
-            data = data.append(file_data, ignore_index=True)
-
-        # Exibe os dados
-        st.write(data)
+    # Exibe os dados
+    st.write(data)
