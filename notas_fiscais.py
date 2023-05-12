8e56a394f9c1c2b49d94abd1a20ec0f1eed05502
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-12 11:18:25 -0300

    Update notas_fiscais.py

--- notas_fiscais.py
+++ notas_fiscais.py
@@ -1,23 +1,16 @@
-# Importando as bibliotecas necessárias
 import streamlit as st
 import pandas as pd
-from bs4 import BeautifulSoup
+
 
 def notas_fiscais():
     # Função para extrair os dados do XML
     def extract_data(file):
-        # Cria um objeto BeautifulSoup com o parser XML
         bs = BeautifulSoup(file, 'xml')
 
-        # Extrai a chave de acesso da nota fiscal
         chave_nfe = bs.find('infProt').get('chNFe') if bs.find('infProt') else None
 
-        # Inicializa uma lista vazia para armazenar os dados de todos os produtos
         data = []
-
-        # Itera sobre todos os elementos <det>
         for det in bs.find_all('det'):
-            # Extrai os dados necessários
             product_data = {
                 'Chave do produto': chave_nfe,
                 'Código de Item': det.get('nItem'),
@@ -32,12 +25,10 @@ def notas_fiscais():
                 'ICMS do produto': det.find('ICMS60').find('vICMSSubstituto').string if det.find('ICMS60') and det.find('ICMS60').find('vICMSSubstituto') else None,
                 'ICMS ret do produto': det.find('ICMS60').find('vICMSSTRet').string if det.find('ICMS60') and det.find('ICMS60').find('vICMSSTRet') else None,
             }
-
-            # Adiciona os dados do produto à lista
             data.append(product_data)
 
         return data
-    # Cria a interface do Streamlit
+
     # Cria a interface do Streamlit
     st.title('Carregador de Notas Fiscais')
 
@@ -46,24 +37,19 @@ def notas_fiscais():
 
     # Se algum ou mais arquivos foram carregados
     if files:
-        # Inicializa uma lista vazia para armazenar os DataFrames individuais de cada arquivo
-        dataframes = []
+        # Inicializa uma lista vazia para armazenar todos os dados
+        all_data = []
 
         # Itera sobre os arquivos carregados
         for file in files:
             # Extrai os dados do arquivo XML
             file_data = extract_data(file)
 
-            # Cria um DataFrame com os dados do arquivo
-            file_df = pd.DataFrame(file_data)
+            # Adiciona os dados do arquivo à lista geral
+            all_data.extend(file_data)
 
-            # Adiciona o DataFrame do arquivo à lista
-            dataframes.append(file_df)
-
-        # Concatena todos os DataFrames em um DataFrame geral
-        data = pd.concat(dataframes, ignore_index=True)
+        # Cria um DataFrame com todos os dados
+        df = pd.DataFrame(all_data)
 
         # Exibe os dados
-        st.write(data)
-
-
+        st.write(df)
