147e1127dc5ac23e88f484cee2d9d8c2bb8493ae
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-12 11:00:29 -0300

    Update notas_fiscais.py

--- notas_fiscais.py
+++ notas_fiscais.py
@@ -3,49 +3,50 @@ import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup
 
+def notas_fiscais():
 # Função para extrair os dados do XML
-def extract_data(file):
-    # Cria um objeto BeautifulSoup
-    bs = BeautifulSoup(file, 'lxml')
-    
-    # Extrai os dados necessários
-    data = {
-        'Chave do produto': bs.find('infprot').get('chnfe') if bs.find('infprot') else None,
-        'Código de Item': bs.find('det').get('nitem') if bs.find('det') else None,
-        'Data de emissão': bs.find('ide').dhemi.string if bs.find('ide') else None,
-        'CFOP do produto': bs.find('prod').cfop.string if bs.find('prod') else None,
-        'NCM do produto': bs.find('prod').ncm.string if bs.find('prod') else None,
-        'Código do produto': bs.find('prod').cprod.string if bs.find('prod') else None,
-        'Descrição do Produto': bs.find('prod').xprod.string if bs.find('prod') else None,
-        'Quantidade do produto': bs.find('prod').qcom.string if bs.find('prod') else None,
-        'EAN do produto': bs.find('prod').cean.string if bs.find('prod') else None,
-        'Valor do Produto': bs.find('prod').vprod.string if bs.find('prod') else None,
-        'ICMS do produto': bs.find('icms60').vicmssubstituto.string if bs.find('icms60') else None,
-        'ICMS ret do produto': bs.find('icms60').vicmsstret.string if bs.find('icms60') else None,
-    }
-    
-    return data
-# Cria a interface do Streamlit
-st.title('Carregador de Notas Fiscais')
+    def extract_data(file):
+        # Cria um objeto BeautifulSoup
+        bs = BeautifulSoup(file, 'lxml')
 
-# Cria um seletor de arquivos para vários arquivos
-files = st.file_uploader('Upload your XML files', type=['xml'], accept_multiple_files=True)
+        # Extrai os dados necessários
+        data = {
+            'Chave do produto': bs.find('infprot').get('chnfe') if bs.find('infprot') else None,
+            'Código de Item': bs.find('det').get('nitem') if bs.find('det') else None,
+            'Data de emissão': bs.find('ide').dhemi.string if bs.find('ide') else None,
+            'CFOP do produto': bs.find('prod').cfop.string if bs.find('prod') else None,
+            'NCM do produto': bs.find('prod').ncm.string if bs.find('prod') else None,
+            'Código do produto': bs.find('prod').cprod.string if bs.find('prod') else None,
+            'Descrição do Produto': bs.find('prod').xprod.string if bs.find('prod') else None,
+            'Quantidade do produto': bs.find('prod').qcom.string if bs.find('prod') else None,
+            'EAN do produto': bs.find('prod').cean.string if bs.find('prod') else None,
+            'Valor do Produto': bs.find('prod').vprod.string if bs.find('prod') else None,
+            'ICMS do produto': bs.find('icms60').vicmssubstituto.string if bs.find('icms60') else None,
+            'ICMS ret do produto': bs.find('icms60').vicmsstret.string if bs.find('icms60') else None,
+        }
 
-# Se algum ou mais arquivos foram carregados
-if files:
-    # Inicializa uma lista vazia para armazenar os dados
-    all_data = []
+        return data
+    # Cria a interface do Streamlit
+    st.title('Carregador de Notas Fiscais')
 
-    # Itera sobre os arquivos carregados
-    for file in files:
-        # Extrai os dados do arquivo XML
-        data = extract_data(file)
+    # Cria um seletor de arquivos para vários arquivos
+    files = st.file_uploader('Upload your XML files', type=['xml'], accept_multiple_files=True)
 
-        # Adiciona os dados à lista
-        all_data.append(data)
+    # Se algum ou mais arquivos foram carregados
+    if files:
+        # Inicializa uma lista vazia para armazenar os dados
+        all_data = []
 
-    # Cria um DataFrame com os dados
-    df = pd.DataFrame(all_data)
+        # Itera sobre os arquivos carregados
+        for file in files:
+            # Extrai os dados do arquivo XML
+            data = extract_data(file)
 
-    # Exibe os dados
-    st.write(df)
+            # Adiciona os dados à lista
+            all_data.append(data)
+
+        # Cria um DataFrame com os dados
+        df = pd.DataFrame(all_data)
+
+        # Exibe os dados
+        st.write(df)
