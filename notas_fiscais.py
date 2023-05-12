7689c765031a8fa56dc0748ecdedce2556f63efa
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-12 09:36:56 -0300

    Update notas_fiscais.py

--- notas_fiscais.py
+++ notas_fiscais.py
@@ -1,128 +1,44 @@
+# Importando as bibliotecas necessárias
 import streamlit as st
 import pandas as pd
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+
 
 def notas_fiscais():
-    st.header("Notas Fiscais")
-    # Adicione aqui o código para a aba "Notas Fiscais"
+# Função para extrair os dados do XML
+    def extract_data(file):
+        # Cria um objeto BeautifulSoup
+        bs = BeautifulSoup(file, 'lxml')
 
-    # Exemplo de localização dos dados no XML
-    xml_data = """
-    <nota_fiscal>
-        <infProt>
-            <chNFe>123456789</chNFe>
-        </infProt>
-        <det>
-            <nItem>1</nItem>
-        </det>
-        <ide>
-            <dhEmi>2023-05-10</dhEmi>
-        </ide>
-        <prod>
-            <CFOP>5102</CFOP>
-            <NCM>1001</NCM>
-            <cProd>ABC123</cProd>
-            <xProd>Produto ABC</xProd>
-            <qCom>10</qCom>
-            <cEAN>7890123456789</cEAN>
-            <vProd>1000.00</vProd>
-        </prod>
-        <ICMS60>
-            <vICMSSubstituto>150.00</vICMSSubstituto>
-            <vICMSSTRet>25.00</vICMSSTRet>
-        </ICMS60>
-    </nota_fiscal>
-    """
+        # Extrai os dados necessários
+        data = {
+            'Chave do produto': bs.find('infprot').get('chnfe'),
+            'Código de Item': bs.find('det').get('nitem'),
+            'Data de emissão': bs.find('ide').dhemi.string,
+            'CFOP do produto': bs.find('prod').cfop.string,
+            'NCM do produto': bs.find('prod').ncm.string,
+            'Código do produto': bs.find('prod').cprod.string,
+            'Descrição do Produto': bs.find('prod').xprod.string,
+            'Quantidade do produto': bs.find('prod').qcom.string,
+            'EAN do produto': bs.find('prod').cean.string,
+            'Valor do Produto': bs.find('prod').vprod.string,
+            'ICMS do produto': bs.find('icms60').vicmssubstituto.string,
+            'ICMS ret do produto': bs.find('icms60').vicmsstret.string,
+        }
 
-    # Parse do XML
-    root = ET.fromstring(xml_data)
+        # Retorna os dados como um DataFrame
+        return pd.DataFrame([data])
 
-    # Extração dos dados do XML
-    chave = root.find(".//chNFe").text
-    item = root.find(".//nItem").text
-    data_emissao = root.find(".//dhEmi").text
-    cfop = root.find(".//CFOP").text
-    ncm = root.find(".//NCM").text
-    codigo_produto = root.find(".//cProd").text
-    descricao = root.find(".//xProd").text
-    quantidade = root.find(".//qCom").text
-    cean = root.find(".//cEAN").text
-    vprod = root.find(".//vProd").text
-    icms_vbcst = root.find(".//vICMSSubstituto").text
-    icms_vbcstret = root.find(".//vICMSSTRet").text
+    # Cria a interface do Streamlit
+    st.title('Carregador de Notas Fiscais')
 
-    # Criação do DataFrame
-    df = pd.DataFrame({
-        "Chave da Nota": [chave],
-        "Item da Nota": [item],
-        "Data de Emissão": [data_emissao],
-        "CFOP": [cfop],
-        "NCM": [ncm],
-        "Código do Produto": [codigo_produto],
-        "Descrição da Nota": [descricao],
-        "Quantidade": [quantidade],
-        "cEAN": [cean],
-        "vProd": [vprod],
-        "ICMS vBCST": [icms_vbcst],
-        "ICMS vBCSTRet": [icms_vbcstret]
-    })
+    # Cria um seletor de arquivos
+    file = st.file_uploader('Upload your XML file', type=['xml'])
 
-    # Exibição do DataFrame
-    st.dataframe(df)
+    # Se um arquivo foi carregado
+    if file is not None:
+        # Extrai os dados do arquivo
+        data = extract_data(file)
 
-# Restante do código para as outras abas
-
-def main():
-    # Configuração do layout
-    st.set_page_config(page_title="Aplicativo de Análise Fiscal", layout="wide")
-
-    # Criação das guias na barra lateral
-    menu_options = ["Notas Fiscais", "Not
-    icms_vbcst = root.find(".//vICMSSubstituto").text
-    icms_vbcstret = root.find(".//vICMSSTRet").text
-
-    # Criação do DataFrame
-    df = pd.DataFrame({
-        "Chave da Nota": [chave],
-        "Item da Nota": [item],
-        "Data de Emissão": [data_emissao],
-        "CFOP": [cfop],
-        "NCM": [ncm],
-        "Código do Produto": [codigo_produto],
-        "Descrição da Nota": [descricao],
-        "Quantidade": [quantidade],
-        "cEAN": [cean],
-        "vProd": [vprod],
-        "ICMS vBCST": [icms_vbcst],
-        "ICMS vBCSTRet": [icms_vbcstret]
-    })
-
-    # Exibição do DataFrame
-    st.dataframe(df)
-
-# Restante do código para as outras abas
-
-def main():
-    # Configuração do layout
-    st.set_page_config(page_title="Aplicativo de Análise Fiscal", layout="wide")
-
-    # Criação das guias na barra lateral
-    menu_options = ["Notas Fiscais", "Notas Complementares", "EFD", "CAT", "Conversão de Código (NF Original)", "Análise Fiscal"]
-    selected_option = st.sidebar.radio("Menu", menu_options)
-
-    # Renderização do conteúdo da guia selecionada
-    if selected_option == "Notas Fiscais":
-        notas_fiscais()
-    elif selected_option == "Notas Complementares":
-        notas_complementares()
-    elif selected_option == "EFD":
-        efd()
-    elif selected_option == "CAT":
-        cat()
-    elif selected_option == "Conversão de Código (NF Original)":
-        conversao_codigo()
-    elif selected_option == "Análise Fiscal":
-        analise_fiscal()
-
-if __name__ == "__main__":
-    main()
+        # Exibe os dados
+        st.write(data)
