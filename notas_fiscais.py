023e4374dca4d7ed9315b539c343053240378ddf
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-12 09:44:59 -0300

    Update notas_fiscais.py

--- notas_fiscais.py
+++ notas_fiscais.py
@@ -3,31 +3,38 @@ import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
 def notas_fiscais():
 # Função para extrair os dados do XML
     def extract_data(file):
         # Cria um objeto BeautifulSoup
         bs = BeautifulSoup(file, 'lxml')
 
-        # Extrai os dados necessários
-        data = {
-            'Chave do produto': bs.find('infprot').get('chnfe'),
-            'Código de Item': bs.find('det').get('nitem'),
-            'Data de emissão': bs.find('ide').dhemi.string,
-            'CFOP do produto': bs.find('prod').cfop.string,
-            'NCM do produto': bs.find('prod').ncm.string,
-            'Código do produto': bs.find('prod').cprod.string,
-            'Descrição do Produto': bs.find('prod').xprod.string,
-            'Quantidade do produto': bs.find('prod').qcom.string,
-            'EAN do produto': bs.find('prod').cean.string,
-            'Valor do Produto': bs.find('prod').vprod.string,
-            'ICMS do produto': bs.find('icms60').vicmssubstituto.string,
-            'ICMS ret do produto': bs.find('icms60').vicmsstret.string,
-        }
+        # Inicializa uma lista vazia para armazenar os dados de todos os produtos
+        data = []
+
+        # Itera sobre todos os elementos <det>
+        for det in bs.find_all('det'):
+            # Extrai os dados necessários
+            product_data = {
+                'Chave do produto': bs.find('infprot').get('chnfe'),
+                'Código de Item': det.get('nitem'),
+                'Data de emissão': bs.find('ide').dhemi.string,
+                'CFOP do produto': det.prod.cfop.string,
+                'NCM do produto': det.prod.ncm.string,
+                'Código do produto': det.prod.cprod.string,
+                'Descrição do Produto': det.prod.xprod.string,
+                'Quantidade do produto': det.prod.qcom.string,
+                'EAN do produto': det.prod.cean.string,
+                'Valor do Produto': det.prod.vprod.string,
+                'ICMS do produto': det.find('icms60').vicmssubstituto.string if det.find('icms60') else None,
+                'ICMS ret do produto': det.find('icms60').vicmsstret.string if det.find('icms60') else None,
+            }
+
+            # Adiciona os dados do produto à lista
+            data.append(product_data)
 
         # Retorna os dados como um DataFrame
-        return pd.DataFrame([data])
+        return pd.DataFrame(data)
 
     # Cria a interface do Streamlit
     st.title('Carregador de Notas Fiscais')
