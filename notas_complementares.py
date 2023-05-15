f9f503feef0fc41280264bcf2a800e33cc30e528
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-15 17:13:54 -0300

    Update notas_complementares.py

--- notas_complementares.py
+++ notas_complementares.py
@@ -3,38 +3,30 @@ import pandas as pd
 
 def notas_complementares():
     # Cria a interface do Streamlit
-    st.title('Carregador de Notas Fiscais Complementares')
+    st.title('Análise de Notas Fiscais')
 
-    # Cria um seletor de arquivos para vários arquivos
-    uploaded_file = st.file_uploader('Upload your XLSX file', type=['xlsx'])
+    # Cria um seletor de arquivos
+    file = st.file_uploader('Upload your Excel file', type=['xlsx'])
 
-    # Se algum arquivo foi carregado
-    if uploaded_file:
-        # Lê os dados do arquivo XLSX
-        df = pd.read_excel(uploaded_file)
+    # Se um arquivo foi carregado
+    if file:
+        df = pd.read_excel(file)
 
-        # Substitui as vírgulas por nada (remove as vírgulas) na coluna 'cProd'
-        df['cProd'] = df['cProd'].str.replace(',', '')
-
-        # Substitui as vírgulas por nada (remove as vírgulas) na coluna 'CFOP'
+        # Substituir vírgulas e pontos
+        df['Código de Produto'] = df['Código de Produto'].str.replace(',', '')
         df['CFOP'] = df['CFOP'].str.replace(',', '')
-
-        # Substitui as vírgulas por pontos na coluna 'NCM'
         df['NCM'] = df['NCM'].str.replace(',', '.')
+        
+        st.write(df)  # Exibir o DataFrame
 
-        # Exibe o dataframe completo
-        st.write(df)
+        # Solicitar ao usuário a chave e o código do produto
+        chave = st.text_input('Digite a Chave Original')
+        codigo_produto = st.text_input('Digite o Código de Produto')
 
-        # Solicita ao usuário para inserir a chave e o código do produto
-        chave = st.text_input("Digite a Chave Original (chNFe)")
-        codigo_produto = st.text_input("Digite o Código do Produto (cProd)")
-
-        # Se o usuário digitou a chave e o código do produto
         if chave and codigo_produto:
-            # Filtra o dataframe para mostrar apenas as linhas que correspondem à chave e ao código do produto
-            filtered_df = df[(df['chNFe'] == chave) & (df['cProd'] == codigo_produto)]
+            # Filtrar o DataFrame com base na chave e no código do produto
+            filtered_df = df[(df['Chave Complementar'] == chave) & (df['Código de Produto'] == codigo_produto)]
             
-            # Exibe os dados filtrados
-            st.write(filtered_df)
+            st.write(filtered_df)  # Exibir o DataFrame filtrado
 
 notas_complementares()
