e46c7e4b7d93b7cfc957f338c59608ccca1f7079
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-15 16:49:14 -0300

    Update notas_complementares.py

--- notas_complementares.py
+++ notas_complementares.py
@@ -1,55 +1,26 @@
 import streamlit as st
 import pandas as pd
-from bs4 import BeautifulSoup
 
 def notas_complementares():
-    # Função para extrair os dados do XML
-    def extract_data(file):
-        bs = BeautifulSoup(file, 'xml')
-
-        chave_nfe = bs.find('chNFe').text if bs.find('chNFe') else None
-        chave_original = bs.find('NFref').find('refNFe').text if bs.find('NFref') and bs.find('NFref').find('refNFe') else None
-
-        data = []
-        for det in bs.find_all('det'):
-            product_data = {
-                'Chave Complementar': chave_nfe,
-                'Chave Original': chave_original,
-                'Código de Item': det.get('nItem'),
-                'Código do produto': det.find('prod').find('cProd').string if det.find('prod') and det.find('prod').find('cProd') else None,
-                'EAN do produto': det.find('prod').find('cEAN').string if det.find('prod') and det.find('prod').find('cEAN') else None,
-                'Descrição do Produto': det.find('prod').find('xProd').string if det.find('prod') and det.find('prod').find('xProd') else None,
-                'NCM do produto': det.find('prod').find('NCM').string if det.find('prod') and det.find('prod').find('NCM') else None,
-                'CFOP do produto': det.find('prod').find('CFOP').string if det.find('prod') and det.find('prod').find('CFOP') else None,
-                'Quantidade do produto': det.find('prod').find('qCom').string if det.find('prod') and det.find('prod').find('qCom') else None,
-                'Valor do Produto': det.find('prod').find('vProd').string if det.find('prod') and det.find('prod').find('vProd') else None,
-                'ICMS Complementar': det.find('ICMS60').find('vICMSSTRet').string if det.find('ICMS60') and det.find('ICMS60').find('vICMSSTRet') else None,
-            }
-            data.append(product_data)
-
-        return data
-
     # Cria a interface do Streamlit
-    st.title('Carregador de Notas Complementares')
+    st.title('Carregador de Notas Fiscais')
 
-    # Cria um seletor de arquivos para vários arquivos
-    files = st.file_uploader('Upload your XML files', type=['xml'], accept_multiple_files=True)
+    # Cria um seletor de arquivos
+    file = st.file_uploader('Upload your Excel file', type=['xlsx'])
 
-    # Se algum ou mais arquivos foram carregados
-    if files:
-        # Inicializa uma lista vazia para armazenar todos os dados
-        all_data = []
+    # Se um arquivo foi carregado
+    if file:
+        # Carrega os dados do arquivo Excel
+        df = pd.read_excel(file)
 
-        # Itera sobre os arquivos carregados
-        for file in files:
-            # Extrai os dados do arquivo XML
-            file_data = extract_data(file)
+        # Solicita ao usuário para inserir a chave e o código do produto
+        chave = st.text_input('Enter the key')
+        codigo_produto = st.text_input('Enter the product code')
 
-            # Adiciona os dados do arquivo à lista geral
-            all_data.extend(file_data)
+        # Filtra o DataFrame com base na chave e no código do produto
+        filtered_df = df[(df['Chave Complementar'] == chave) & (df['Código do produto'] == codigo_produto)]
 
-        # Cria um DataFrame com todos os dados
-        df = pd.DataFrame(all_data)
+        # Exibe os dados filtrados
+        st.write(filtered_df)
 
-        # Exibe os dados
-        st.write(df)
+notas_fiscais()
