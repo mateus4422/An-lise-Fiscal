6ddf85b6e65c080a54206c036cf90e070cf1d119
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-15 16:58:10 -0300

    Update notas_complementares.py

--- notas_complementares.py
+++ notas_complementares.py
@@ -3,24 +3,26 @@ import pandas as pd
 
 def notas_complementares():
     # Cria a interface do Streamlit
-    st.title('Carregador de Notas Fiscais')
+    st.title('Carregador de Notas Fiscais Complementares')
 
-    # Cria um seletor de arquivos
-    file = st.file_uploader('Upload your Excel file', type=['xlsx'])
+    # Cria um seletor de arquivos para vários arquivos
+    file = st.file_uploader('Upload your XLSX file', type=['xlsx'])
 
-    # Se um arquivo foi carregado
+    # Se algum arquivo foi carregado
     if file:
-        # Carrega os dados do arquivo Excel
+        # Lê os dados do arquivo XLSX
         df = pd.read_excel(file)
 
         # Solicita ao usuário para inserir a chave e o código do produto
-        chave = st.text_input('Enter the key')
-        codigo_produto = st.text_input('Enter the product code')
+        chave = st.text_input("Digite a Chave Complementar (chNFe)")
+        codigo_produto = st.text_input("Digite o Código do Produto (cProd)")
 
-        # Filtra o DataFrame com base na chave e no código do produto
-        filtered_df = df[(df['Chave Complementar'] == chave) & (df['Código do produto'] == codigo_produto)]
-
-        # Exibe os dados filtrados
-        st.write(filtered_df)
+        # Se o usuário digitou a chave e o código do produto
+        if chave and codigo_produto:
+            # Filtra o dataframe para mostrar apenas as linhas que correspondem à chave e ao código do produto
+            filtered_df = df[(df['chNFe'] == chave) & (df['cProd'] == codigo_produto)]
+            
+            # Exibe os dados
+            st.write(filtered_df)
 
 notas_complementares()
