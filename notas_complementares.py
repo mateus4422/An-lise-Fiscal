03783bf048c89617f800dfbf764514c51dd42d20
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-15 17:01:26 -0300

    Update notas_complementares.py

--- notas_complementares.py
+++ notas_complementares.py
@@ -6,15 +6,18 @@ def notas_complementares():
     st.title('Carregador de Notas Fiscais Complementares')
 
     # Cria um seletor de arquivos para vários arquivos
-    file = st.file_uploader('Upload your XLSX file', type=['xlsx'])
+    uploaded_file = st.file_uploader('Upload your XLSX file', type=['xlsx'])
 
     # Se algum arquivo foi carregado
-    if file:
+    if uploaded_file:
         # Lê os dados do arquivo XLSX
-        df = pd.read_excel(file)
+        df = pd.read_excel(uploaded_file)
+
+        # Exibe o dataframe completo
+        st.write(df)
 
         # Solicita ao usuário para inserir a chave e o código do produto
-        chave = st.text_input("Digite a Chave Complementar (chNFe)")
+        chave = st.text_input("Digite a Chave Original (chNFe)")
         codigo_produto = st.text_input("Digite o Código do Produto (cProd)")
 
         # Se o usuário digitou a chave e o código do produto
@@ -22,7 +25,7 @@ def notas_complementares():
             # Filtra o dataframe para mostrar apenas as linhas que correspondem à chave e ao código do produto
             filtered_df = df[(df['chNFe'] == chave) & (df['cProd'] == codigo_produto)]
             
-            # Exibe os dados
+            # Exibe os dados filtrados
             st.write(filtered_df)
 
 notas_complementares()
