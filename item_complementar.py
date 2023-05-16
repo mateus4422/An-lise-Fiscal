61d339f43380c1de59ddd36e11d7f93a8440cc0f
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-16 17:38:08 -0300

    Add files via upload

---
+++ item_complementar.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+def read_xlsx(file):
+    return pd.read_excel(file)
+
+st.title("Leitor de Arquivos Excel")
+
+uploaded_file = st.file_uploader("Selecione o arquivo Excel:", type=["xlsx"])
+
+if uploaded_file is not None:
+    df = read_xlsx(uploaded_file)
+    st.write(df)
