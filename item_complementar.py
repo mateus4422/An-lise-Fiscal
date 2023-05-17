aed34bde70dfacba00b1e6f50863e40d4a6e91bf
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-17 10:36:17 -0300

    Update item_complementar.py

--- item_complementar.py
+++ item_complementar.py
@@ -3,7 +3,12 @@ import pandas as pd
 
 def item_complementar():
     def read_xlsx(file):
-        return pd.read_excel(file)
+        df = pd.read_excel(file)
+
+        # Remova quaisquer caracteres não numéricos e converta para int
+        df['cprod'] = df['cprod'].str.replace(',', '').str.extract('(\d+)', expand=False).astype(int)
+
+        return df
 
     st.title("Leitor de Arquivos Excel")
 
@@ -11,4 +16,9 @@ def item_complementar():
 
     if uploaded_file is not None:
         df = read_xlsx(uploaded_file)
+        
+        # Renomeie as colunas
+        df.rename(columns={df.columns[0]: 'Chave', df.columns[1]: 'Chave + Código + Ítem'}, inplace=True)
+
         st.write(df)
+
